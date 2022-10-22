c4349f86bce53c2c761c61d69780c27555a8b563
Jan <jannupoiss007@gmail.com>
2022-10-22 13:05:27 +0300

    levelite vahetumine

--- Area2D.gd
+++ Area2D.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 
 func _on_flag_body_entered(_body):
-	var _x=get_tree().change_scene("res://Youwin.tscn")
+	var _x=get_tree().change_scene("res://Level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
 	
